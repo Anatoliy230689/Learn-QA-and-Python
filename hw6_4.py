0be827bb0bacf6611e78c50565a4735cce6b6b81
Anatoliy <33453820+Anatoliy230689@users.noreply.github.com>
2021-04-21 14:52:12 +0300

    Update hw6_4.py

--- hw6_4.py
+++ hw6_4.py
@@ -3,7 +3,7 @@ import smtplib
 smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 smtpObj.starttls()
 print(type(smtpObj))
-smtpObj.login('maklabas23@gmail.com', '12345Qqq.')
+smtpObj.login('maklabas23@gmail.com', '******')
 
 smtpObj.sendmail('maklabas23@gmail.com','anatoliy.babiy1@gmail.com', 'Test email' )
 smtpObj.quit()
