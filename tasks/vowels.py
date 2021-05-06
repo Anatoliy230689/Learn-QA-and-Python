324e1e2279394ecbda4d72a4a4f36835f9768be5
Anatoliy230689 <anatoliy.babiy1@gmail.com>
2021-05-06 20:26:46 +0300

    add change

--- tasks/vowels.py
+++ tasks/vowels.py
@@ -3,14 +3,10 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = input("Provide a word to search for vowels: ")
 found = {}
-found['a'] = 0
-found['e'] = 0
-found['i'] = 0
-found['o'] = 0
-found['u'] = 0
-
 for letter in word:
     if letter in vowels:
+        found.setdefault(letter, 0)  # если буквы нет в словаре то она инициализируется как ключ
         found[letter] += 1
+
 for k, v in sorted(found.items()): # Цыкл перебора ключа и значения в отсортированом списке
     print(k, 'was found', v, 'time(s).')
