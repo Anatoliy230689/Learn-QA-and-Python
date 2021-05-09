87cb519a45349e8a779ce7f6d8383437966899bd
Anatoliy230689 <anatoliy.babiy1@gmail.com>
2021-05-09 21:58:15 +0300

    Flask update

--- tasks/flask/hello_flask.py
+++ tasks/flask/hello_flask.py
@@ -4,6 +4,10 @@ app = Flask (__name__)
 
 @app.route('/')
 def hello() -> str:
-    return  'Hello world from Flask!'
+    return  'Hello world)'
+
+@app.route('/search4')
+def do_search()-> str:
+    return 'Search4'
 
 app.run()
